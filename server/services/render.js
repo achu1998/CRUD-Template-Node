0b792324de77bb6997758277f7a0f9de91d47b5f
Ashween <ashweenr1998@gmail.com>
2022-11-06 03:40:52 +0530

    New commit

---
+++ server/services/render.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv')
+dotenv.config({path:'config.env'})
+const axios = require('axios')
+
+exports.homeRoutes = (req, res) => {
+    // Make a get request to /api/users
+    axios.get(process.env.RELEASE_ENV+'api/users')
+        .then(function(response){
+            res.render('index', { users : response.data , siteUrl : process.env.RELEASE_ENV})
+        })
+        .catch(err =>{
+            res.send(err)
+        })
+
+    
+}
+
+exports.add_user = (req, res) =>{
+    res.render('add_user')
+}
+
+exports.update_user = (req, res) =>{
+    axios.get(process.env.RELEASE_ENV+'api/users', { params : { id : req.query.id }})
+        .then(function(userdata){
+            res.render("update_user", { user : userdata.data, siteUrl : process.env.RELEASE_ENV})
+        })
+        .catch(err =>{
+            res.send(err);
+        })
+}
